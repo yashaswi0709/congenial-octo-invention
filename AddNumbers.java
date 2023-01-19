01f919e5f8a949310a49b9f06d57216fba8bf69f
yashaswi0709 <yashaswipatel0709@gmail.com>
2023-01-19 06:53:38 +0530

    sum of 2 digit number

---
+++ AddNumbers.java
@@ -0,0 +1,8 @@
+public class AddNumbers {
+    public static void main(String[] args) {
+      int num1 = 5, num2 = 15, sum;
+      sum = num1 + num2;
+      System.out.println("Sum of these numbers: "+sum);
+    }
+  }
+
